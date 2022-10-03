cef7785eedd9786ea8566a6862688b7f322b1455
Julien Senopati <113386522+jolansnop@users.noreply.github.com>
2022-10-03 07:21:33 -0600

    I got stuck I do not enjoy Java

--- src/java/models/Note.java
+++ src/java/models/Note.java
@@ -1,9 +1,44 @@
 package models;
-
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Serializable;
 /**
  *
  * @author jsenopati
  */
-public class Note {
+public class Note implements Serializable {
+    private String title;
+    private String content;
     
+    public Note(String path) throws FileNotFoundException, IOException{
+        BufferedReader br = new BufferedReader(new FileReader (new File(path)));
+        title = br.readLine();
+        content = br.readLine();
+        br.close();
+    }
+    
+    public Note(String title, String content, String path) throws IOException {
+        this.title = title;
+        this.content = content;
+        
+        PrintWriter pw = new PrintWriter(new BufferedWriter
+        (new FileWriter(path, false)));
+        pw.println(title);
+        pw.println(content);
+        pw.close();
+    }
+    
+    public String getTitle(){
+        return title;
+    }
+    
+    public String getContent(){
+        return content;
+    }
 }
--- src/java/servlets/NoteServlet.java
+++ src/java/servlets/NoteServlet.java
@@ -1,10 +1,14 @@
 package servlets;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import models.Note;
 
 /**
  *
@@ -15,6 +19,18 @@ public class NoteServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        String whatPage = request.getParameter("edit");
+        String path = getServletContext().getRealPath("/WEB-INF/note.txt");
+        request.setAttribute("note", new Note(path) );
+        
+        if (whatPage != null){
+            getServletContext().getRequestDispatcher("/WEB-INF/editnote.jsp")
+                    .forward(request,response);
+        } else {
+            getServletContext().getRequestDispatcher("/WEB-INF/viewnote.jsp")
+                    .forward(request,response);
+        }
+        
     }
 
     @Override
